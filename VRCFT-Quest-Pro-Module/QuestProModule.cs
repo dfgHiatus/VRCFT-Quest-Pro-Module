cf8ca442b75e7dab0ae4dc976f11618d872c2617
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-10-27 09:02:20 -0500

    Started buffer experiments

--- VRCFT-Quest-Pro-Module/QuestProModule.cs
+++ VRCFT-Quest-Pro-Module/QuestProModule.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
@@ -13,22 +12,14 @@ namespace VRCFT_Quest_Pro_Module
 
         private UdpClient listener;
         private IPEndPoint groupEP;
-        private Dictionary<FBExpression, float> expressions = new Dictionary<FBExpression, float>();
-        private byte[] buffer = new byte[63 * 4];
-        private byte[] slice = new byte[4];
-
+        private float[] expressions = new float[63 * 4];
+        
         public override (bool SupportsEye, bool SupportsLip) Supported => (true, true);
         
         public override (bool eyeSuccess, bool lipSuccess) Initialize(bool eye, bool lip)
         {
             listener = new UdpClient(listenPort);
             groupEP = new IPEndPoint(IPAddress.Any, listenPort);
-            
-            foreach (FBExpression expression in Enum.GetValues(typeof(FBExpression)))
-            {
-                expressions.Add(expression, 0f);
-            }
-
             return (true, true);
         }
         
@@ -50,15 +41,10 @@ namespace VRCFT_Quest_Pro_Module
             {
                 // We start by receiving the data from the UDP listener.
                 // The data is a byte array 63*4 bytes long, since floats are 32 bits long
-                buffer = listener.Receive(ref groupEP);
-                for (int i = 0; i < buffer.Length; i += 4)
-                {
-                    slice[0] = buffer[i];
-                    slice[1] = buffer[i + 1];
-                    slice[2] = buffer[i + 2];
-                    slice[3] = buffer[i + 3];
-                    expressions[(FBExpression)i] = BitConverter.ToSingle(slice, 0);
-                }
+                Buffer.BlockCopy(listener.Receive(ref groupEP), 0, expressions, 0, 63 * 4);
+
+                // Usage:
+                // expressions[FBExpression.Cheek_Suck_L]
             }
             catch (SocketException e)
             {
